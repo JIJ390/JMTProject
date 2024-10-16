ce2540e6c4edc0b0d1c259edb1bcd4167e628ff9
DESKTOP-NVUI4N0\sungm <sungmin48601@gmail.com>
2024-10-16 13:37:58 +0900

    no message

--- src/main/java/edu/kh/jmt/restaurant/controller/RestaurantController.java
+++ src/main/java/edu/kh/jmt/restaurant/controller/RestaurantController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import edu.kh.jmt.restaurant.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
