532b21d933868c62068bfdf84fb11a0bd91af0a6
ykhong1107 <nada_yk@naver.com>
2024-10-18 17:27:54 +0900

    24.10.18 보드연습

---
+++ src/main/java/edu/kh/jmt/board/controller/BoardController.java
@@ -0,0 +1,45 @@
+package edu.kh.jmt.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.test.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("board")
+public class BoardController {
+
+	private final BoardService service;
+	
+	@GetMapping("boardMain")
+	public String boardMain() {
+		
+		
+		return "/board/boardMain";
+	}
+	
+	
+	
+	
+	@GetMapping("boardWrite")
+	public String boardWrite() {
+		
+		return "/board/boardWrite";
+	}
+	
+	
+	@GetMapping("boardComment")
+	public String boardComment() {
+		
+		return "/board/boardComment";
+		
+		
+		
+	}
+	
+}
